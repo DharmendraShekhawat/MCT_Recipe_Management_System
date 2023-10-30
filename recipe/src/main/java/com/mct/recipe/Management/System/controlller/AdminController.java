71d5a6d9e453d87f135c76ca733a046ff1668eb0
Dharmendra Shekhawat <dharmendrashekhawat1403@gmail.com>
2023-10-30 18:49:17 +0530

    my fault

--- recipe/src/main/java/com/mct/recipe/Management/System/controlller/AdminController.java
+++ recipe/src/main/java/com/mct/recipe/Management/System/controlller/AdminController.java
@@ -2,13 +2,17 @@ package com.mct.recipe.Management.System.controlller;
 
 import com.mct.recipe.Management.System.entity.Admin;
 import com.mct.recipe.Management.System.service.AdminService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
 
+
 @RestController
+@Validated
 public class AdminController {
 
     @Autowired
@@ -16,19 +20,19 @@ public class AdminController {
 
     //-------------- admin SignUp------------------------
     @PostMapping("/admin")
-    public ResponseEntity<String> adminSignUp(@RequestBody Admin admin) throws NoSuchAlgorithmException {
+    public ResponseEntity<String> adminSignUp(@Valid @RequestBody Admin admin) throws NoSuchAlgorithmException {
         return adminService.adminSignup(admin);
     }
 
     //-------------- admin SignIn------------------------
     @PostMapping("/admin/signIn")
-    public ResponseEntity<String> adminSignIn(@RequestParam String adminEmail,@RequestParam String adminPassword) {
+    public ResponseEntity<String> adminSignIn(@Valid @RequestParam String adminEmail,@Valid @RequestParam String adminPassword) {
         return adminService.adminSignIn(adminEmail,adminPassword);
     }
 
     //-------------- admin SignOut------------------------
     @DeleteMapping("/admin/signOut")
-    public ResponseEntity<String> adminSignOut(@RequestParam String adminEmail,@RequestParam String tokenValue) {
+    public ResponseEntity<String> adminSignOut(@Valid @RequestParam String adminEmail,@Valid @RequestParam String tokenValue) {
         return adminService.adminSignOut(adminEmail,tokenValue);
     }
 
