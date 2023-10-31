86bb6e4a911e25aa680a9aa15e65d04693d4c41c
Dharmendra Shekhawat <dharmendrashekhawat1403@gmail.com>
2023-10-31 10:07:42 +0530

    first commit

--- recipe/src/main/java/com/mct/recipe/Management/System/controlller/RecipeController.java
+++ recipe/src/main/java/com/mct/recipe/Management/System/controlller/RecipeController.java
@@ -3,14 +3,17 @@ package com.mct.recipe.Management.System.controlller;
 import com.mct.recipe.Management.System.entity.Recipe;
 import com.mct.recipe.Management.System.service.AdminService;
 import com.mct.recipe.Management.System.service.RecipeService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Validated
 public class RecipeController {
 
     @Autowired
@@ -21,25 +24,25 @@ public class RecipeController {
 
     //-------------- add recipe by authenticated admin----------------------
     @PostMapping("/recipe")
-    public ResponseEntity<String> addRecipe(@RequestParam String email,@RequestParam String tokenValue, @RequestBody Recipe recipe){
+    public ResponseEntity<String> addRecipe(@Valid  @RequestParam String email,@Valid @RequestParam String tokenValue,@Valid @RequestBody Recipe recipe){
         return recipeService.addRecipe(email,tokenValue, recipe);
     }
 
     //-------------- get all recipes by authenticated admin----------------------
     @GetMapping("/recipe")
-    public ResponseEntity<List<Recipe>> getAllRecipe(@RequestParam String email, @RequestParam String tokenValue){
+    public ResponseEntity<List<Recipe>> getAllRecipe(@Valid @RequestParam String email,@Valid @RequestParam String tokenValue){
         return recipeService.getAllRecipe(email,tokenValue);
     }
 
     //-------------- add recipe by authenticated admin----------------------
     @PutMapping("/recipe/name/ingredients")
-    public ResponseEntity<String> updateRecipe(@RequestParam String email,@RequestParam String tokenValue, @RequestBody Recipe recipe){
+    public ResponseEntity<String> updateRecipe(@Valid @RequestParam String email,@Valid @RequestParam String tokenValue,@Valid @RequestBody Recipe recipe){
         return recipeService.updateRecipe(email,tokenValue, recipe);
     }
 
     //-------------- delete recipe with recipeId by authenticated admin----------------------
     @DeleteMapping("/recipe")
-    public ResponseEntity<String> deleteRecipe(@RequestParam String email,@RequestParam String tokenValue,@PathVariable Integer recipeId){
+    public ResponseEntity<String> deleteRecipe(@Valid @RequestParam String email,@Valid @RequestParam String tokenValue,@Valid @PathVariable Integer recipeId){
         return recipeService.deleteRecipe(email,tokenValue, recipeId);
     }
 
